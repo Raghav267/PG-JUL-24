c8fcc28352bc1e16961c622d70ebd05263015272
Raghav267 <harshraghav089@gmail.com>
2024-07-11 11:27:18 +0530

    Fix some issues

--- E-Commerce-v3/middleware.js
+++
@@ -1,8 +0,0 @@
-
-module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.flash('error', 'You need to login first!');
-        return res.redirect('/auth/login');
-    }
-    next();
-}
