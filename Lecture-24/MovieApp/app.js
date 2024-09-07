c82c65400e2a9420e66baa382135c43848ee8533
Raghav267 <harshraghav089@gmail.com>
2024-09-07 22:22:26 +0530

    React Intro

--- Lecture-24/MovieApp/app.js
+++ Lecture-24/MovieApp/app.js
@@ -1,12 +1,30 @@
 const form = document.querySelector("#form");
+const result = document.querySelector('#result')
 
 async function getTvShows(searchText) {
-    const res = await axios.get("https://api.tvmaze.com/singlesearch/shows?q=" + searchText);
-    console.log(res);
+    const res = await axios.get("https://api.tvmaze.com/search/shows?q=" + searchText);
+    console.log(res.data);
+    for (let item of res.data) {
+        if (item.show.image) {
+            const imgurl = item.show.image.medium;
+            const image = document.createElement("img");
+            image.src = imgurl;
+            image.style.margin = "10px";
+            result.append(image);
+        }
+    }
+}
+
+function removeSearchResult() {
+    while (result.firstChild) {
+        result.firstChild.remove();
+    }
 }
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const searchText = (event.srcElement[0].value);
+    removeSearchResult();
     getTvShows(searchText);
+    event.srcElement[0].value = "";
 })
---
+++ Lecture-25/first-react-app/src/App.js
@@ -0,0 +1,17 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+function App() {
+  return (
+/*#__PURE__*/_jsxs("div", {
+    children: [/*#__PURE__*/_jsx("h1", {
+      children: "This is some other h1"
+    }), /*#__PURE__*/_jsx("h1", {
+      children: "Hello from app.js"
+    })]
+  })
+
+  )
+}
+
+export default App;
+
+
