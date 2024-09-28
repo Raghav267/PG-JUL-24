7bb71b5eb56f137012338a8a9be3ecb549c6b4f1
Raghav267 <harshraghav089@gmail.com>
2024-09-28 22:15:02 +0530

    Templating

---
+++ Lecture-33/Templating/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const ejs = require('ejs')
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const fruits = [
+    "Mangoes",
+    "Apples",
+    "Grapes",
+    "Litchi"
+]
+
+app.get("/", (req, res) => {
+    // res.send("We are learning Templating.")
+    res.render("welcome");
+})
+
+app.get("/index", (req, res) => {
+    res.render("index");
+})
+
+app.get("/fruit", (req, res) => {
+    res.render("fruit", { fruits });
+})
+
+app.listen(3000, () => {
+    console.log("Server start running at 3000 port.");
+})
